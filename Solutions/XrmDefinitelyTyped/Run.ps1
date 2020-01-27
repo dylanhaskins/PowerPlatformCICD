3906219afefd0b899929fab0bce459d312f5d9d4
Eugene van Staden <eugene.vanstaden@intergen.co.nz>
2020-01-27 19:33:16 +1300

    test account.ts Fix version on XrmDefinintelyTyped

--- Solutions/XrmDefinitelyTyped/Run.ps1
+++ Solutions/XrmDefinitelyTyped/Run.ps1
@@ -1,11 +1,1 @@
-if (!$Credentials)
-{
-	$Credentials = Get-Credential
-}
-if (!$username)
-{
-$username =  $Credentials.GetNetworkCredential().UserName
-$password =  $Credentials.GetNetworkCredential().Password
-}
-
-.\XrmDefinitelyTyped.exe /username:$username /password:$password /useconfig
+.\XrmDefinitelyTyped.exe
--- WebResources/src/ts/forms/account.ts
+++ WebResources/src/ts/forms/account.ts
@@ -1,1 +1,11 @@
 ﻿
+
+namespace Demo.Account {
+  var Form: Form.account.Main.Account;
+
+  export function onLoad(executionContext: Xrm.ExecutionContext<any>) {
+    Form = <Form.account.Main.Account>executionContext.getFormContext();
+    // Code here..
+    Form.getAttribute("firstname");
+  }
+}
