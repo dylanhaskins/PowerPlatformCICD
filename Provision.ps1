3a11a1b975eab9512fff511f432c4e412464d145
Dylan Haskins <dylan.haskins@intergen.co.nz>
2020-01-31 09:00:44 +1300

    Branch management Updates.

--- Provision.ps1
+++ Provision.ps1
@@ -120,8 +120,8 @@ $adAppCreds = az ad app credential reset --password $azureADAppPassword --id $ad
 chdir -Path \Dev\Repos\$adoRepo\
 
 git checkout $branch
-git branch -r | select-string -notmatch $branch | select-string -notmatch HEAD | foreach { git push origin --delete ("$_").Replace("origin/","").Trim()} #Remove non-used branches from remote
-git branch | select-string -notmatch $branch | foreach {git branch -D ("$_").Trim()} #Remove non-used local branches
+#git branch -r | select-string -notmatch $branch | select-string -notmatch HEAD | foreach { git push origin --delete ("$_").Replace("origin/","").Trim()} #Remove non-used branches from remote
+#git branch | select-string -notmatch $branch | foreach {git branch -D ("$_").Trim()} #Remove non-used local branches
 
 Remove-Item .git -Recurse -Force
 git init
@@ -243,7 +243,7 @@ $connCICD = Connect-CrmOnlineDiscovery -Credential $Credentials
 
 git add -A
 git commit -m "Initial Commit"
-git push origin $branch --force
+git push origin master --force
 
 
 $varGroup = az pipelines variable-group create --name "$adoRepo.D365DevEnvironment"  --variables d365username=$username --authorize $true | ConvertFrom-Json
