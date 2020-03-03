a3cf6f6990275d5b03fc28e735d0525ad1be0180
Dylan Haskins <dylan@haskins.co.nz>
2020-03-03 15:45:20 +1300

    Added ExportType to PackageDeployer

--- PackageDeployer/MasterPackage.cs
+++ PackageDeployer/MasterPackage.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Xrm.Tooling.PackageDeployment.CrmPackageExtentionBase;
+using System.ComponentModel.Composition;
 
 namespace PackageDeployer
 {
+    [Export(typeof(IImportExtensions))]
     public class MasterPackage : ImportExtension
     {
         public override string GetLongNameOfImport => "Master Package";
