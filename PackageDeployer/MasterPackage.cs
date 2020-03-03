88842f8b68f85a12440eddfdd98a0b4697ded2e2
Dylan Haskins <dylan@haskins.co.nz>
2020-03-03 14:58:07 +1300

    CleanUp of PackageDeployment (to support newer versions of tooling)

---
+++ PackageDeployer/MasterPackage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xrm.Tooling.PackageDeployment.CrmPackageExtentionBase;
+
+namespace PackageDeployer
+{
+    public class MasterPackage : ImportExtension
+    {
+        public override string GetLongNameOfImport => "Master Package";
+
+        public override string GetImportPackageDataFolderName => "PkgFolder";
+
+        public override string GetImportPackageDescriptionText => "Master Package";
+
+        public override bool AfterPrimaryImport()
+        {
+            return true;
+        }
+
+        public override bool BeforeImportStage()
+        {
+            return true;
+        }
+
+        public override string GetNameOfImport(bool plural)
+        {
+            return GetLongNameOfImport;
+        }
+
+        public override void InitializeCustomExtension()
+        {
+            //Do Nothing
+        }
+    }
+}
