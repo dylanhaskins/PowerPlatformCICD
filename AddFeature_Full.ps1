f13571fd04835e795b257fea959b16524f4c33d4
Dylan Haskins <dylan@haskins.co.nz>
2020-03-05 09:56:39 +1300

    Set-Location

--- AddFeature_Full.ps1
+++ AddFeature_Full.ps1
@@ -207,13 +207,13 @@ $ProgressBar = New-BTProgressBar -Status $message -Value 0.30
 New-BurntToastNotification -Text $Text -ProgressBar $ProgressBar -Silent -UniqueIdentifier $UniqueId
 
 git clone https://github.com/dylanhaskins/FeatureTemplate.git $chosenSolution
-chdir -Path .\$chosenSolution
+Set-Location -Path .\$chosenSolution
 
 git checkout $Branch
 git branch | select-string -notmatch $Branch | foreach {git branch -D ("$_").Trim()} #Remove non-used local branches
 
 Remove-Item .git -Recurse -Force
-cd ..
+Set-Location -Path .\..
 
 $message = "Setting Configurations in Source Code"
 Write-Host $message
