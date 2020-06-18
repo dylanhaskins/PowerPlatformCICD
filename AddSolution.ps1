ff2d648d527b433d6a5a298bc283129aed1b84e0
Dylan Haskins <dylan@haskins.co.nz>
2020-06-19 07:26:30 +1200

    Update AddSolution.ps1

--- AddSolution.ps1
+++ AddSolution.ps1
@@ -3,13 +3,6 @@ Param(
     [string] [Parameter(Mandatory = $false)] $FeatureBranch = "master"
 )
 
-#$sourceFile = Invoke-WebRequest "https://raw.githubusercontent.com/dylanhaskins/PowerPlatformCICD/$branch/AddSolution.ps1" -UseBasicParsing:$true
-#Set-Content .\AddSolution.ps1 -Value $sourceFile.Content
-
-#$sourceFile = Invoke-WebRequest "https://raw.githubusercontent.com/dylanhaskins/PowerPlatformCICD/$branch/AddSolution_Full.ps1" -UseBasicParsing:$true
-#Set-Content .\AddSolution_Full.ps1 -Value $sourceFile.Content
-
 Start-Sleep -Seconds 2
 
-& .\AddSolution_Full.ps1 -ErrorAction Stop #-Branch $FeatureBranch
-
+& .\AddSolution_Full.ps1 -ErrorAction Stop 
