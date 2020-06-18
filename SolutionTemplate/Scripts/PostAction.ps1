fc628515f4fcfeb339358e29f5c0200c26cbe697
Dylan Haskins <dylan@haskins.co.nz>
2020-06-19 00:08:51 +1200

    Updates to Pre and PostAction

--- SolutionTemplate/Scripts/PostAction.ps1
+++ SolutionTemplate/Scripts/PostAction.ps1
@@ -1,9 +1,3 @@
 ﻿######################## SETUP 
 $ProgressPreference = 'SilentlyContinue'
 . (Join-Path $PSScriptRoot "DeploymentFunctions.ps1")
-
-    #Update Staff ID of Users
-    Set-StaffID -Conn $CRMConn
-
-	#Delete False Name Name Type
-	Remove-FalseNameType -Conn $CRMConn
